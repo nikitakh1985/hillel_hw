5dde2e3c767ab3552bba114f61ef861ecba6703e
nikitakh1985 <nikita.kalchevskiy@gmail.com>
2019-11-28 13:11:33 +0200

    add hw2

---
+++ HW2/aven&odd.java
@@ -0,0 +1,16 @@
+public class test {
+    public static void main(String[] args) {
+        int[] a = {2, 3, 4, 5, 9, 33, 66, 97, 27, 10};
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] % 2 == 0) {
+                System.out.println("even=" + a[i] + " ");
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] % 2 != 0) {
+                System.out.println("odd=" + a[i] + " ");
+            }
+        }
+    }
+}
