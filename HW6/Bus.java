3ddb6096d4e54117cae319ec9125e6460e2324ac
nikitakh1985 <nikita.kalchevskiy@gmail.com>
2019-12-22 06:27:12 +0200

    add HW6

---
+++ HW6/Bus.java
@@ -0,0 +1,27 @@
+package HW6;
+
+public class Bus extends Car {
+    private int numberOfSeats;
+
+    public Bus(int id, String mark, int price, int fuelConsumption, int maxSpeed, int numberOfSeats) {
+        super(id, mark, price, fuelConsumption, maxSpeed);
+        this.numberOfSeats = numberOfSeats;
+    }
+
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    public void setNumberOfSeats(int numberOfSeats) {
+        this.numberOfSeats = numberOfSeats;
+    }
+
+
+    @Override
+    public String toString() {
+        return "ID: " + getid() +" " + getmark() + "  " + "Bus [numberOfSeats="
+                + numberOfSeats + ", price=" + getPrice() +
+                ", fuelConsumption=" + getFuelConsumption() + ", maxSpeed=" + getMaxSpeed() + "]";
+    }
+
+}
