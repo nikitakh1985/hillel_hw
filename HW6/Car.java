7ad0752f179939eef198066e118cf758fe944eed
nikitakh1985 <nikita.kalchevskiy@gmail.com>
2019-12-22 06:33:25 +0200

    add HW6

---
+++ HW6/Car.java
@@ -0,0 +1,61 @@
+package HW6;
+
+public class Car {
+    private int id;
+    private int price;
+    private int fuelConsumption;
+    private int maxSpeed;
+    private String mark;
+
+    public Car(int id, String mark, int price, int fuelConsumption, int maxSpeed) {
+        this.id = id;
+        this.mark = mark;
+        this.price = price;
+        this.fuelConsumption = fuelConsumption;
+        this.maxSpeed = maxSpeed;
+    }
+
+    public Car(int id, String mark, int price, int maxSpeed) {
+        this(id, mark, price, 0, maxSpeed);
+    }
+
+    public int getid() {
+        return id;
+    }
+
+    public String getmark() {
+        return mark;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setid(int id) {
+        this.id = id;
+    }
+
+    public void setmark(String mark) {
+        this.mark = mark;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getFuelConsumption() {
+        return fuelConsumption;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setFuelConsumption(int fuelConsumption) {
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+}
---
+++ HW6/ElectricCar.java
@@ -0,0 +1,31 @@
+package HW6;
+
+public class ElectricCar extends Car {
+    private int powerReserve;
+
+    public ElectricCar(int id, String mark, int price, int maxSpeed, int powerReserve) {
+        super(id, mark, price, maxSpeed);
+        this.powerReserve = powerReserve;
+
+    }
+
+    public int getPowerReserve() {
+        return powerReserve;
+    }
+
+    public void setPowerReserve(int powerReserve) {
+        this.powerReserve = powerReserve;
+    }
+
+    @Override
+    public int getFuelConsumption() {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + getid() + " " + getmark() + " " + "ElectricCar [powerReserve=" + powerReserve +
+                ", prises=" + getPrice() + ", maxSpeed=" + getMaxSpeed() + "]";
+
+    }
+}
